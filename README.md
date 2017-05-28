5b3e3f7a71556241fb7b69c645578d474a111f9f
AnaTofuZ <e155730@ie.u-ryukyu.ac.jp>
2017-05-28 12:26:52 +0900

    git init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::Want5000trillion - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Want5000trillion;
+
+# DESCRIPTION
+
+Acme::Want5000trillion is ...
+
+# LICENSE
+
+Copyright (C) AnaTofuZ.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+AnaTofuZ <e155730@ie.u-ryukyu.ac.jp>
---
+++ lib/Acme/Want5000trillion.pm
@@ -0,0 +1,39 @@
+package Acme::Want5000trillion;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Want5000trillion - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Want5000trillion;
+
+=head1 DESCRIPTION
+
+Acme::Want5000trillion is ...
+
+=head1 LICENSE
+
+Copyright (C) AnaTofuZ.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+AnaTofuZ E<lt>e155730@ie.u-ryukyu.ac.jpE<gt>
+
+=cut
+
